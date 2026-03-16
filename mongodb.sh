51aa0b94b792911c6440d1962f7bff2be01f6c75
ChandiniTatikonda <chandinitatikonda1114@gmail.com>
2026-03-16 20:19:55 +0530

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
 
-source ./common.sh
-
-Check_root
-
-echo "MongoDB installation started"
-
-dnf install mongodb -y &>>$LOGS_FILE
-VALIDATE $? "MongoDB Installation"
-
-print_total_time
